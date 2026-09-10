2544e5562d18c7d959a4baeddea73dd341a714a1
uxzane6225 <132722108+uxzane6225@users.noreply.github.com>
2026-09-10 21:12:35 +0800

    Revert "Shrunk font size from 20 to 18 for result content"
    This reverts commit e695da4266de728b24511b034dd2dd53b2f48ee9.

--- lib/pages/components/result_content.dart
+++ lib/pages/components/result_content.dart
@@ -13,13 +13,13 @@ class ResultContent extends StatelessWidget {
         Text(
           "$title: ",
           style: TextStyle(
-            fontSize: 18,
+            fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
         Text(
           content ?? "No Pokemon yet",
-          style: TextStyle(fontSize: 18),
+          style: TextStyle(fontSize: 20),
         ),
       ],
     );
