037a41eeccb64199414a0d938f605f942cee8100
uxzane6225 <132722108+uxzane6225@users.noreply.github.com>
2026-09-10 19:45:56 +0800

    Added packages directory and other subdirectories within that directory. Added Pokemon model in the packages directory's models folder

---
+++ packages/pokeapi_api/lib/serc/models/pokemon.dart
@@ -0,0 +1,17 @@
+class Pokemon {
+  final int id;
+  final String name;
+  final int baseExperience;
+  final int height;
+  final int order;
+  final int weight;
+
+  Pokemon({
+    required this.id,
+    required this.name,
+    required this.baseExperience,
+    required this.height,
+    required this.order,
+    required this.weight,
+  });
+}
